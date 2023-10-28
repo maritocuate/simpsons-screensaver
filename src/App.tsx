03e0cdbbf14514e78982233a124e37436ff4a797
maritocuate <mario300@gmail.com>
2023-10-28 12:58:11 -0300

    chore: add quote component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.scss'
+import Quote from './component/Quote'
 
 function App() {
   return (
     <>
-      <h1>
-        "<span>Big</span> <span>yikes</span> <span>cool</span>{' '}
-        <span>always</span>"
-      </h1>
+      <Quote text="big yikes cool always" author="Homer Simpson" />
     </>
   )
 }
---
+++ src/component/Quote/index.tsx
@@ -0,0 +1,29 @@
+import './index.scss'
+
+interface QuoteProps {
+  text: string
+  author: string
+}
+
+const normalizeText = (text: string) => {
+  return text.charAt(0).toUpperCase() + text.slice(1).toLocaleLowerCase()
+}
+
+const Quote = ({ text, author }: QuoteProps) => {
+  const formattedQuote = normalizeText(text)
+  const splittedQuote = formattedQuote.split(' ')
+  const formattedAuthor = author.toUpperCase()
+  return (
+    <>
+      <h1 id="quote">
+        "
+        {splittedQuote.map((word, i) => (
+          <span key={i}>{word}</span>
+        ))}
+        "
+      </h1>
+      <p>{formattedAuthor}</p>
+    </>
+  )
+}
+export default Quote
